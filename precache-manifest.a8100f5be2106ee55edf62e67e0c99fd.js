cd7250a3503dbc3c90a737b54769a91bf4a30f0e
vilmacio22 <vilmacio22@users.noreply.github.com>
2020-08-13 11:40:42 -0300

    Updates

---
+++ precache-manifest.a8100f5be2106ee55edf62e67e0c99fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a125cc61b8245f26791fe24d9c5fa0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a319934685c8b33d65",
+    "url": "/static/css/main.e3a988f1.chunk.css"
+  },
+  {
+    "revision": "2f14afb55c16b6cb17af",
+    "url": "/static/js/2.e09e3550.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.e09e3550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a319934685c8b33d65",
+    "url": "/static/js/main.eef838dd.chunk.js"
+  },
+  {
+    "revision": "50918aee25a1e5182297",
+    "url": "/static/js/runtime-main.932e20d9.js"
+  }
+]);
